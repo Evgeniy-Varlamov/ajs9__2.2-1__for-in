50b2577da74b072161ed832f4b168d2e4f1dee27
Evgeniy Varlamov <Varlamov-EO@ya.ru>
2020-04-30 00:32:48 +0400

    update orderByProps

--- src/orderByProps.js
+++ src/orderByProps.js
@@ -1,27 +1,27 @@
 
 export default function orderByProps(obj, sortFields) {
-  const result = [];
+  const arrObjs = [];
   for (const key in obj) {
     if (Object.prototype.hasOwnProperty.call(obj, key)) {
-      result.push({
+      arrObjs.push({
         key,
         value: obj[key],
       });
     }
   }
-  result.sort((a, b) => {
-    if (a.key > b.key) {
-      return 1;
-    }
-    return -1;
-  });
   for (let i = sortFields.length - 1; i >= 0; i -= 1) {
-    result.sort((a) => {
-      if (a.key == sortFields[i]) {
+    arrObjs.sort((a) => {
+      if (a.key === sortFields[i]) {
         return -1;
       }
       return 0;
     });
   }
+  const arrObjsTemp = arrObjs.splice(sortFields.length);
+  arrObjsTemp.sort((a, b) => {
+    if (a.key > b.key) return 1;
+    return -1;
+  });
+  const result = arrObjs.concat(arrObjsTemp);
   return result;
 }
