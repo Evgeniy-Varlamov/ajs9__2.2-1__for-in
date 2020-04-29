7ab0ca451005bbf96f57544182e9ed1eed67b707
Evgeniy Varlamov <Varlamov-EO@ya.ru>
2020-04-30 00:44:33 +0400

    update orderByProps

--- src/orderByProps.js
+++ src/orderByProps.js
@@ -1,28 +1,28 @@
-
-export default function orderByProps(obj, sortFields) {
-  const arrObjs = [];
-  for (const key in obj) {
-    if (Object.prototype.hasOwnProperty.call(obj, key)) {
-      arrObjs.push({
-        key,
-        value: obj[key],
-      });
+export default function orderByProps(objInput, sortValue) {
+  const arrObj = [];
+  for (const key in objInput) {
+    if (Object.prototype.hasOwnProperty.call(objInput, key)) {
+      arrObj.push(
+        {
+          key,
+          value: objInput[key],
+        },
+      );
     }
   }
-  for (let i = sortFields.length - 1; i >= 0; i -= 1) {
-    arrObjs.sort((a) => {
-      if (a.key === sortFields[i]) {
-        return -1;
-      }
+
+  for (let i = sortValue.length - 1; i > -1; i -= 1) {
+    arrObj.sort((a) => {
+      if (a.key === sortValue[i]) return -1;
       return 1;
     });
   }
-  const arrObjsTemp = arrObjs.splice(sortFields.length);
-  arrObjsTemp.sort((a, b) => {
+
+  const arrTemp = arrObj.splice(sortValue.length);
+  arrTemp.sort((a, b) => {
     if (a.key > b.key) return 1;
     return -1;
   });
-  const result = arrObjs.concat(arrObjsTemp);
-  console.log(result);
-  return result;
+  const arrReturn = arrObj.concat(arrTemp);
+  return arrReturn;
 }
